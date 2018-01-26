23cb9b2778ca3bc5c38d60d8005205a24e0afa2c
Andrea Bravaccino <andreabravaccino@gmail.com>
2018-01-26 14:30:49 +0100

    sistemato il barchart

--- src/main/java/view/chartsViews/BarChartClass.java
+++ src/main/java/view/chartsViews/BarChartClass.java
@@ -17,6 +17,7 @@ public class BarChartClass implements Observer, ChartInterface {
     private BarChart barChart;
     private List<XYChart.Data<String, Number>> datas;
     private XYChart.Series<String, Number> series;
+    private String[] colors = {"navy", "lightskyblue", "lightslategray", "lightsalmon", "limegreen"};
 
     public BarChartClass(BarChart barChart, int index) {
         this.barChart = barChart;
@@ -39,7 +40,6 @@ public class BarChartClass implements Observer, ChartInterface {
                 barChart.setTitle("Biglietti venduti per Location " + String.valueOf(newValue));
             }
         });
-
         BarChartModel.getInstance().addObserver(this);
     }
 
@@ -52,27 +52,22 @@ public class BarChartClass implements Observer, ChartInterface {
 
             if (barChart.getData().isEmpty()) {
                 datas = new ArrayList<>();
-
-                for (int i = 0; i < settori.size(); i++) {
-                    datas.add(new XYChart.Data(nomeSettori.get(i), settori.get(nomeSettori.get(i))));
-                }
-                for (XYChart.Data<String, Number> data : datas) {
-                    series.getData().add(data);
-                }
-
-                barChart.getData().add(series);
             } else {
-                series.getData().clear();
-                datas.clear();
-
-                for (int i = 0; i < settori.size(); i++) {
-                    datas.add(new XYChart.Data(nomeSettori.get(i), settori.get(nomeSettori.get(i))));
-                }
-
-                for (XYChart.Data<String, Number> data : datas) {
-                    series.getData().add(data);
-                }
+                barChart.getData().clear();
             }
+            series.getData().clear();
+            datas.clear();
+
+            for (int i = 0; i < settori.size(); i++) {
+                datas.add(new XYChart.Data(nomeSettori.get(i), settori.get(nomeSettori.get(i))));
+            }
+
+            for (XYChart.Data<String, Number> data : datas) {
+                setRandomColor(data);
+                series.getData().add(data);
+            }
+            barChart.getData().add(series);
+
         } else {
             BarChartModel barChartModel = (BarChartModel) o;
             HashMap<Integer, String> locationIdMap = barChartModel.getLocationIdMap();
@@ -80,32 +75,30 @@ public class BarChartClass implements Observer, ChartInterface {
             List<Integer> soldPerLocation = barChartModel.getSoldPerLocation();
             if (barChart.getData().isEmpty()) {
                 datas = new ArrayList<>();
-
-                for (int i = 0; i < locationIdMap.size(); i++) {
-                    datas.add(new XYChart.Data(locationNames.get(i), soldPerLocation.get(i).intValue()));
-                }
-                for (XYChart.Data<String, Number> data : datas) {
-                    series.getData().add(data);
-                }
-
-                barChart.getData().add(series);
             } else {
-                series.getData().clear();
-                datas.clear();
-
-                for (int i = 0; i < locationIdMap.size(); i++) {
-                    datas.add(new XYChart.Data(locationNames.get(i), soldPerLocation.get(i).intValue()));
-                }
-
-                for (XYChart.Data<String, Number> data : datas) {
-                    series.getData().add(data);
-                }
+                barChart.getData().clear();
             }
+            series.getData().clear();
+            datas.clear();
+
+            for (int i = 0; i < locationIdMap.size(); i++) {
+                datas.add(new XYChart.Data(locationNames.get(i), soldPerLocation.get(i).intValue()));
+            }
+            for (XYChart.Data<String, Number> data : datas) {
+                setRandomColor(data);
+
+                series.getData().add(data);
+            }
+            barChart.getData().add(series);
         }
     }
 
     @Override
     public void initializeCharts() {
+        barChart.setAnimated(false);
+        barChart.getXAxis().setAnimated(false);
+        barChart.getYAxis().setAnimated(false);
+        barChart.legendVisibleProperty().setValue(false);
         if (barChart.getData().isEmpty()) {
             series = new XYChart.Series<>();
         } else {
@@ -115,5 +108,15 @@ public class BarChartClass implements Observer, ChartInterface {
                 datas.add(series.getData().get(i));
             }
         }
+    }
+
+    private void setRandomColor(XYChart.Data<String, Number> data) {
+        int randomIndex = new Random().nextInt(colors.length);
+        String randomColor = (colors[randomIndex]);
+        data.nodeProperty().addListener((ov, oldNode, newNode) -> {
+            if (newNode != null) {
+                newNode.setStyle("-fx-bar-fill: " + randomColor + ";");
+            }
+        });
     }
 }
