0cdcac3d4bbfebb494dd22f9c45a62723ec3b153
Andrea Bravaccino <andreabravaccino@gmail.com>
2018-02-08 13:43:48 +0100

    aggiunta eliminazione immagini nell'inserimento dell'evento

--- src/main/java/view/SlideShowView.java
+++ src/main/java/view/SlideShowView.java
@@ -3,8 +3,11 @@ package view;
 import controller.SlideShowController;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -12,15 +15,12 @@ import model.EventListModel;
 import model.EventModel;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
 /**
  * View che si occupa dello slideshow di immagini
- *
+ * <p>
  * implementa observer come secondo architettura MVC
  */
 public class SlideShowView implements Observer {
@@ -51,20 +51,18 @@ public class SlideShowView implements Observer {
     private List<Integer> activeList;
 
     /**
-     *
      * Costruttore usato per la shashboard
      *
-     *
-     * @param hBox hbox contenente i bottoni
-     * @param dashBoardSlideShowLeftButton bottone sinistro dello slideshow
+     * @param hBox                          hbox contenente i bottoni
+     * @param dashBoardSlideShowLeftButton  bottone sinistro dello slideshow
      * @param dashBoardSlideShowRightButton bottone destro dello slideshow
-     * @param slideShowController istanza del controller
+     * @param slideShowController           istanza del controller
      */
     public SlideShowView(HBox hBox, Button dashBoardSlideShowLeftButton,
                          Button dashBoardSlideShowRightButton, SlideShowController slideShowController) {
         eventListModel.addObserver(this); //setto come observer della lista di eventi
         this.slideShowController = slideShowController; //valorizzo l'instanza locale di slideshowcontroller
-        this.hBox=hBox; //vsetto l'hbox
+        this.hBox = hBox; //vsetto l'hbox
         buttonList = new ArrayList<>(); //Instanzio le liste di bottoni
         activeList = new ArrayList<>();
         //listener per il bottone destro
@@ -83,7 +81,7 @@ public class SlideShowView implements Observer {
             for (int i = 0; i < 8; i++) {
                 Button button = new Button(); //instanzio il nuovo bottone
                 buttonList.add(button); //lo aggiungo alla lista dei bottoni
-                if (i<4){
+                if (i < 4) {
                     hBox.getChildren().add(button); //se l'indice è minore di 4 verrà reso visibile
                     activeList.add(i); //e aggiunto alla lista dei bottoni attivi
                 }
@@ -92,25 +90,24 @@ public class SlideShowView implements Observer {
     }
 
     /**
-     *
      * Costruttore usato per la eventView
      *
-     * @param hBox hbox contenente i bottoni
-     * @param dashBoardSlideShowLeftButton bottone sinistro dello slideshow
+     * @param hBox                          hbox contenente i bottoni
+     * @param dashBoardSlideShowLeftButton  bottone sinistro dello slideshow
      * @param dashBoardSlideShowRightButton bottone destro dello slideshow
-     * @param slideShowController istanza del controller
-     * @param eventModel //instanza del model a cui la schermata eventView e quindi lo slideshow fa riferimento
+     * @param slideShowController           istanza del controller
+     * @param eventModel                    //instanza del model a cui la schermata eventView e quindi lo slideshow fa riferimento
      */
     public SlideShowView(HBox hBox, Button dashBoardSlideShowLeftButton,
                          Button dashBoardSlideShowRightButton, SlideShowController slideShowController, EventModel eventModel) {
-        int i=0; // 
+        int i = 0; //
         this.eventModel = eventModel;
         eventModel.addObserver(this);
         buttonList = new ArrayList<>();
         activeList = new ArrayList<>();
 
         this.slideShowController = slideShowController;
-        this.hBox=hBox;
+        this.hBox = hBox;
 
         dashBoardSlideShowRightButton.setOnAction(event -> {
             right();
@@ -119,8 +116,8 @@ public class SlideShowView implements Observer {
             left();
         });
         if (hBox != null) {
-            while (hBox.getChildren().size() > 0){
-                   hBox.getChildren().remove(hBox.getChildren().size() -1);
+            while (hBox.getChildren().size() > 0) {
+                hBox.getChildren().remove(hBox.getChildren().size() - 1);
             }
             hBox.setAlignment(Pos.CENTER);
             hBox.setSpacing(20);
@@ -132,7 +129,7 @@ public class SlideShowView implements Observer {
                 imageView.setFitWidth(300.0);
                 imageView.setFitHeight(280.0);
                 buttonList.get(i).setGraphic(imageView);
-                if (i<4){
+                if (i < 4) {
                     hBox.getChildren().add(button);
                     activeList.add(i);
                 }
@@ -140,13 +137,13 @@ public class SlideShowView implements Observer {
         }
     }
 
-    public SlideShowView( HBox hBox, Button left, Button right, SlideShowController slideShowController, List<Image> immagini) {
-        int i=0; //
+    public SlideShowView(HBox hBox, Button left, Button right, SlideShowController slideShowController, List<Image> immagini) {
+        int i = 0; //
         buttonList = new ArrayList<>();
         activeList = new ArrayList<>();
 
         this.slideShowController = slideShowController;
-        this.hBox=hBox;
+        this.hBox = hBox;
 
         right.setOnAction(event -> {
             right();
@@ -162,25 +159,35 @@ public class SlideShowView implements Observer {
             for (i = 0; i < immagini.size(); i++) {
                 Button button = new Button();
                 buttonList.add(button);
+                buttonList.add(button);
+                if (i < 4) {
+                    hBox.getChildren().add(button);
+                    activeList.add(i);
+                }
+                int finalI = i;
+                button.setOnAction(event -> {
+                    if (deleteConfirm()) {
+                        buttonList.remove(button);
+                        activeList.remove(finalI);
+                        immagini.remove(finalI);
+                        hBox.getChildren().remove(button);
+                    }
+                });
                 ImageView imageView = new ImageView(immagini.get(i));
                 imageView.setFitWidth(300.0);
                 imageView.setFitHeight(280.0);
                 buttonList.get(i).setGraphic(imageView);
-                if (i<4){
-                    hBox.getChildren().add(button);
-                    activeList.add(i);
-                }
             }
         }
     }
 
 
     private void right() {
-        if (activeList.get(3)< (buttonList.size() -1)){
-            activeList.set(0, activeList.get(0)+1);
-            activeList.set(1, activeList.get(1)+1);
-            activeList.set(2, activeList.get(2)+1);
-            activeList.set(3, activeList.get(3)+1);
+        if (activeList.get(3) < (buttonList.size() - 1)) {
+            activeList.set(0, activeList.get(0) + 1);
+            activeList.set(1, activeList.get(1) + 1);
+            activeList.set(2, activeList.get(2) + 1);
+            activeList.set(3, activeList.get(3) + 1);
             hBox.getChildren().remove(3);
             hBox.getChildren().remove(2);
             hBox.getChildren().remove(1);
@@ -193,12 +200,12 @@ public class SlideShowView implements Observer {
 
     }
 
-    private void left(){
-        if (activeList.get(0)>0){
-            activeList.set(0, activeList.get(0)-1);
-            activeList.set(1, activeList.get(1)-1);
-            activeList.set(2, activeList.get(2)-+1);
-            activeList.set(3, activeList.get(3)-1);
+    private void left() {
+        if (activeList.get(0) > 0) {
+            activeList.set(0, activeList.get(0) - 1);
+            activeList.set(1, activeList.get(1) - 1);
+            activeList.set(2, activeList.get(2) - +1);
+            activeList.set(3, activeList.get(3) - 1);
             hBox.getChildren().remove(3);
             hBox.getChildren().remove(2);
             hBox.getChildren().remove(1);
@@ -229,8 +236,8 @@ public class SlideShowView implements Observer {
                 });
             }
         } else {
-            int i=0;
-            while (i < eventModel.getSlideshow().size()){
+            int i = 0;
+            while (i < eventModel.getSlideshow().size()) {
                 Image image = eventModel.getSlideshow().get(i);
                 ImageView imageView = new ImageView(image);
                 imageView.setFitWidth(300.0);
@@ -241,6 +248,16 @@ public class SlideShowView implements Observer {
             }
 
         }
+    }
+
+    private boolean deleteConfirm() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Eliminazione immagine");
+        alert.setHeaderText("Eliminazione Immagine");
+        alert.setContentText("Eliminare definitivamente l'immagine?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.get() == ButtonType.OK;
     }
 
 }
