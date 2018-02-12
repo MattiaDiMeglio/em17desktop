de39892bc68e4e8b9fbc66ab731308e8883274c6
Andrea Bravaccino <andreabravaccino@gmail.com>
2018-02-12 10:40:57 +0100

    commit

--- src/main/java/controller/SlideShowController.java
+++ src/main/java/controller/SlideShowController.java
@@ -8,36 +8,80 @@ import view.SlideShowView;
 
 import java.util.List;
 
+/**
+ * controller per la creazione degli slideshow
+ *
+ * @author ingSW20
+ */
 public class SlideShowController {
+    /**
+     * variabile per l'utilizzo di {@link InsertController}
+     *
+     * @see #setImageList(List)
+     */
     private InsertController insertController;
+    /**
+     * Variabile per l'utilizzo di {@link ViewSourceController}
+     *
+     * @see #handler(int)
+     */
     private ViewSourceController viewSourceController;
 
-    public void createSlide(HBox hBox, Button dashBoardSlideShowLeftButton,
-                            Button dashBoardSlideShowRightButton, ViewSourceController viewSourceController){
-        new SlideShowView(hBox, dashBoardSlideShowLeftButton, dashBoardSlideShowRightButton, this);
+    /**
+     * metodo per la creazione dello slideshow all'interno della {@link view.DashBoardView DashBoardView}
+     *
+     * @param hBox                 HBox contenente le immagini
+     * @param leftButton           bottone per scorrere la lista di immagini verso sinistra
+     * @param rightButton          bottone per scorrere la lista di immagini verso destra
+     * @param viewSourceController variabile per valorizzare {@link #viewSourceController}
+     */
+    public void createSlide(HBox hBox, Button leftButton,
+                            Button rightButton, ViewSourceController viewSourceController) {
+        new SlideShowView(hBox, leftButton, rightButton, this);
         this.viewSourceController = viewSourceController;
     }
 
-    public void createSlide (HBox hBox, Button dashBoardSlideShowLeftButton,
-                             Button dashBoardSlideShowRightButton, ViewSourceController viewSourceControl, EventModel eventModel){
-        SlideShowView slideShowView = new SlideShowView(hBox, dashBoardSlideShowLeftButton, dashBoardSlideShowRightButton, this, eventModel);
-        this.viewSourceController = viewSourceController;
+    /**
+     * metodo per la creazione dello slideshow all'interno di {@link view.EventView EventView}
+     *
+     * @param hBox        HBox contenente le immagini
+     * @param leftButton  bottone per scorrere la lista di immagini verso sinistra
+     * @param rightButton bottone per scorrere la lista di immagini verso destra
+     * @param eventModel  model contenente le informazioni dell'evento da visualizzare
+     */
+    public void createSlide(HBox hBox, Button leftButton,
+                            Button rightButton, EventModel eventModel) {
+        new SlideShowView(hBox, leftButton, rightButton, this, eventModel);
     }
 
-    public void createSlide(InsertController insertController, Button left, HBox hbox, Button right, List<Image> immagini) {
+    /**
+     * @param hbox             HBox contenente le immagini
+     * @param insertController variabile per la valorizzazione di {@link #insertController}
+     * @param leftButton       bottone per scorrere la lista di immagini verso sinistra
+     * @param rightButton      bottone per scorrere la lista di immagini verso destra
+     * @param immagini         lista con le immagini da impostare nello slideshow
+     */
+    public void createSlide(InsertController insertController, Button leftButton, HBox hbox, Button rightButton, List<Image> immagini) {
         this.insertController = insertController;
-        SlideShowView slideShowView = new SlideShowView( hbox, left, right, this, immagini);
-        this.viewSourceController = viewSourceController;
+        new SlideShowView(hbox, leftButton, rightButton, this, immagini);
     }
 
-
+    /**
+     * metodo per passare alla view per la visualizzazione di evento specifico
+     *
+     * @param index indice identificativo dell'evento
+     */
     public void handler(int index) {
         viewSourceController.toEventView(index);
     }
 
-    public void setImageList(List<Image> imageList){
+    /**
+     * Il metodo setta all'interno di {@link InsertController} la imagelist
+     * che verrà caricata sul server
+     *
+     * @param imageList lista di immagini che verrà caricata sul server
+     */
+    public void setImageList(List<Image> imageList) {
         insertController.setImagesList(imageList);
     }
-
-
 }
